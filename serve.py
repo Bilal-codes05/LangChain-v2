ba1ebdfc40b18ac73af3550671c064b52cfef821
Bilal-Rafique-coder <chbilalrafique2@gmail.com>
2025-07-21 11:43:34 +0500

    LangChain v2

---
+++ serve.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+from dotenv import load_dotenv
+import os 
+os.environ["LANGCHAIN_PROJECT"] = "LangChain v2"
+load_dotenv()
+
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = ChatGroq(
+    model_name="llama3-8b-8192",
+    max_tokens=1024,
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# 5. Adding chain route
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
